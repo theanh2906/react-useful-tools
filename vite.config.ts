a3b9cbbfe175a85b31fa4ee9dc17bcfbaf882d7c
Anh Tang <anh.tang@opswat.com>
2026-02-02 14:57:59 +0700

    feat: add Firebase configuration file and update vite.config to load env files

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir: './environments', // Load env files from environments/ directory
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
